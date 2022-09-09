80a7e5af165d06bd42707cb44f485fd9f70e99dd
Reiziger <ashu.reiziger45@gmail.com>
2022-09-08 19:40:14 -0700

    puts Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main-print the string in the puts function
+*Description: write a C program that prints exactly
+*Programming is like building a multilingual puzzle
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
