d8756e6ea9f9e565d14c4cd1d585bc4cb1e0a363
Reiziger <ashu.reiziger45@gmail.com>
2022-09-30 18:45:06 -0700

    what is my name

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file: main.h
+ * Auth: Ashu Reiziger Achuo
+ * Desc: Header file containing prototypes
+ * for all functions used in 0x0A C argc, argv
+ */
+
+int _putchar(char c);
+int check_num(char *str);
+
+#endif
